b6ffa27b49bfc6f192b6d717ef9214a8839c63e4
王磊 <xljhs1015617751@163.com>
2023-02-18 11:49:47 +0800

    feat: 初始化工程

---
+++ .commitlintrc.js
@@ -0,0 +1,12 @@
+/**
+ * @Description
+ * @Author 王猛龙（王磊）
+ * @Date 2023-02-18 11:27:41
+ * @LastEditors 王猛龙（王磊）
+ * @LastEditTime 2023-02-18 11:27:50
+ * @Email xljhs1015617751@163.com
+ * @Phone 166 4357 5302
+ */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
---
+++ packages/test.js
@@ -0,0 +1,10 @@
+/**
+ * @Description 测试代码校验
+ * @Author 王猛龙（王磊）
+ * @Date 2023-02-17 17:21:55
+ * @LastEditors 王猛龙（王磊）
+ * @LastEditTime 2023-02-18 11:45:12
+ * @Email xljhs1015617751@163.com
+ * @Phone 166 4357 5302
+ */
+const a = 111;
